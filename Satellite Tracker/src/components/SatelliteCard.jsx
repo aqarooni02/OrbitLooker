b2183c8bf1c7a09f1a1e54e697a1133d5efd8b1a
Ahmed Qarooni <aaq2002@gmail.com>
2025-04-13 18:59:47 +0300

    added more info using api and norad ID

--- Satellite Tracker/src/components/SatelliteCard.jsx
+++ Satellite Tracker/src/components/SatelliteCard.jsx
@@ -1,13 +1,25 @@
 export const SatelliteCard = ({ satellite }) => {
     return (
-        <div className="card">
-            {satellite && <>
-                <h3>Name: {satellite.name}</h3>
-                <h3>NORAD ID: {satellite.noradId}</h3>
-            </>
-            }
-
-
+        <div className="card" style={{ padding: "1rem", border: "1px solid #ccc", borderRadius: "8px" }}>
+            {satellite ? (
+                <>
+                    <h3>{satellite.name}</h3>
+                    <p><strong>NORAD ID:</strong> {satellite.noradId}</p>
+                    {satellite.launchDate && (
+                        <>
+                            <p><strong>Launch Date:</strong> {satellite.launchDate}</p>
+                            <p><strong>Owener:</strong> {satellite.owner}</p>
+                            <p><strong>Object Type:</strong> {satellite.objectType}</p>
+                            <p><strong>Apogee (km):</strong> {satellite.apogee}</p>
+                            <p><strong>Perigee (km):</strong> {satellite.perigee}</p>
+                            <p><strong>Inclination (°):</strong> {satellite.inclination}</p>
+                            <p><strong>Launch Site:</strong> {satellite.launchSite}</p>
+                        </>
+                    )}
+                </>
+            ) : (
+                <p>Select a satellite to see more details.</p>
+            )}
         </div>
-    )
-}
+    );
+};
